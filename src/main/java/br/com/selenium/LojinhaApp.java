ebaf7833abf0796762ee4ed81cb3acc34bb29650
MaksonVia <109090857+MaksonVia@users.noreply.github.com>
2023-11-24 18:16:33 -0300

    Update project

--- src/main/java/br/com/selenium/LojinhaApp.java
+++ src/main/java/br/com/selenium/LojinhaApp.java
@@ -1,7 +1,8 @@
 package br.com.selenium;
 
 public class LojinhaApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Olá Mundo");
     }
 }
